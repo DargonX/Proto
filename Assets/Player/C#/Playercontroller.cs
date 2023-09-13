596c5cf7dcd0b399cc80c02c67a6c95a84e468da
DargonX <86307582+DargonX@users.noreply.github.com>
2023-09-13 23:42:30 +0200

    Actualization 0.5
    ep. https://www.youtube.com/watch?v=HUSTzMlPy20&t=688s

--- Assets/Player/C#/Playercontroller.cs
+++ Assets/Player/C#/Playercontroller.cs
@@ -6,18 +6,36 @@ using UnityEngine.InputSystem;
 public class Playercontroller : MonoBehaviour
 {
 
-    [field: SerializeField] public float MoveSpeed { get; private set; } = 5f;
+    [field: SerializeField] public float MoveForce { get; private set; }
+    [field: SerializeField] public DirectionalAnimationSet Idle { get; private set; }
+
     private Vector2 _axisInput = Vector2.zero;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private DirectionalAnimationSet _currentSet;
+    private AnimationClip _currentClip;
+    private Vector2 _facingDir;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _currentSet = Idle;
+    }
+
+    private void Update()
+    {
+        AnimationClip expectedClip = _currentSet.GetFcingClip(_facingDir);
+
+        if(_currentClip == null || _currentClip != expectedClip)
+        {
+            _animator.Play(expectedClip.name);
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveForce = _axisInput * MoveSpeed * Time.fixedDeltaTime;
+        Vector2 moveForce = _axisInput * MoveForce * Time.fixedDeltaTime;
 
         _rb.AddForce(moveForce);
     }
@@ -25,6 +43,11 @@ public class Playercontroller : MonoBehaviour
     private void OnMove(InputValue value)
     {
         _axisInput = value.Get<Vector2>();
+
+        if(_axisInput != Vector2.zero)
+        {
+            _facingDir = _axisInput;
+        }
     }
 
     private void OnUse(InputValue value)
