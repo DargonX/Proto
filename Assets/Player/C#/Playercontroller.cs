bb00fbc0945f4d2abbc285a62a44930b9d1a53f5
DargonX <86307582+DargonX@users.noreply.github.com>
2023-09-15 00:59:16 +0200

    Actualization 0.7
    ep. https://www.youtube.com/watch?v=2H5bJNvYlb8&t=135s

--- Assets/Player/C#/Playercontroller.cs
+++ Assets/Player/C#/Playercontroller.cs
@@ -12,6 +12,24 @@ public class Playercontroller : MonoBehaviour
     [field: SerializeField] public CHaracterState Use { get; private set; }
 
     [field: SerializeField] public StateAnimationDictionary StateAnimation { get; private set; }
+    [field: SerializeField] public float WalkVelocityTreshold { get; private set; } = 0.05f;
+
+    public CHaracterState CurrentState
+    {
+        get
+        {
+            return _currentState;
+        }
+        set
+        {
+            if(_currentState != value)
+            {
+                _currentState = value;
+                ChangeClip();
+                _timeToEndAnimation = _currentClip.length;
+            }
+        }
+    }
 
     private Vector2 _axisInput = Vector2.zero;
     private Rigidbody2D _rb;
@@ -19,29 +37,52 @@ public class Playercontroller : MonoBehaviour
     private CHaracterState _currentState;
     private AnimationClip _currentClip;
     private Vector2 _facingDir;
+    private float _timeToEndAnimation = 0f;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-        _currentState = Idle;
+        CurrentState = Idle;
     }
 
     private void Update()
     {
+        _timeToEndAnimation = Mathf.Max(_timeToEndAnimation - Time.deltaTime, 0);
+
+        if (_currentState.CanExitWhilePlaying || _timeToEndAnimation <= 0)
+        {
+            if (_axisInput != Vector2.zero && _rb.velocity.magnitude > WalkVelocityTreshold)
+            {
+                CurrentState = Walk;
+            }
+            else
+            {
+                CurrentState = Idle;
+            }
+
+            ChangeClip();
+        }
+    }
+
+    private void ChangeClip()
+    {
         AnimationClip expectedClip = StateAnimation.GetFacingClipFromState(_currentState, _facingDir);
 
-        if(_currentClip == null || _currentClip != expectedClip)
+        if (_currentClip == null || _currentClip != expectedClip)
         {
             _animator.Play(expectedClip.name);
+            _currentClip = expectedClip;
         }
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveForce = _axisInput * MoveForce * Time.fixedDeltaTime;
-
-        _rb.AddForce(moveForce);
+        if (_currentState.CanMove)
+        {
+            Vector2 moveForce = _axisInput * MoveForce * Time.fixedDeltaTime;
+            _rb.AddForce(moveForce);
+        }
     }
 
     private void OnMove(InputValue value)
@@ -56,6 +97,6 @@ public class Playercontroller : MonoBehaviour
 
     private void OnUse(InputValue value)
     {
-
+        CurrentState = Use;
     }
 }
